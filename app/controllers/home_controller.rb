03cd69746a2fb5503f5ec8da3b9d7e1b1076f31a
Tim Upchurch <timup@Tims-MacBook-Pro-4.local>
2015-06-29 18:45:56 -0400

    add post method to clover class

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,27 @@
 require 'clover'
 
 class HomeController < ApplicationController
+
+
   def index
     @clover = Clover.new("#{current_user.access_token}", "#{current_user.merchant_id}")
-
+    @item_names = []
+    @clover.items['elements'].each do |item|
+      @item_names.push(item['name'])
+    end
     # @response = HTTParty.get("https://api.clover.com/v3/merchants/#{current_user.merchant_id}/items?access_token=#{current_user.access_token}")
+  end
+
+  def orders
+    @clover = Clover.new("#{current_user.access_token}", "#{current_user.merchant_id}")
+
+    @order_title = params["order_title"]
+    @new_orders = @clover.new_order(@order_title)
+  end
+
+  private
+  # for before_action filter later
+  def set_clover
+    @clover = Clover.new("#{current_user.access_token}", "#{current_user.merchant_id}")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  get 'orders' => 'home#orders'
+  post 'orders' => 'home#orders'
+
   devise_for :users, controllers: {
         :omniauth_callbacks => "users/omniauth_callbacks"
       }
